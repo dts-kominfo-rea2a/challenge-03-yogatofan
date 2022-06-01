60bab4937bdcb583f11dc9f08e110abf813eb8f7
Yoga Ari Tofan <y.aritofan@gmail.com>
2022-06-02 00:56:30 +0700

    challenge: JS Fungsi

--- index.js
+++ index.js
@@ -1,46 +1,67 @@
 // ! JANGAN DIMOFIDIKASI
 const dataBelanjaan = [
-  {
-    id: "D-10001",
-    nama: "Minyak Goreng Delima",
-    harga: 25000,
-    kuantitas: 2,
-  },
-  {
-    id: "D-10020",
-    nama: "Beras Mamos",
-    harga: 48000,
-    kuantitas: 1,
-  },
-  {
-    id: "F-00010",
-    nama: "Larutan Cap Kaki Empat",
-    harga: 7500,
-    kuantitas: 8,
-  },
+	{
+		id: "D-10001",
+		nama: "Minyak Goreng Delima",
+		harga: 25000,
+		kuantitas: 2,
+	},
+	{
+		id: "D-10020",
+		nama: "Beras Mamos",
+		harga: 48000,
+		kuantitas: 1,
+	},
+	{
+		id: "F-00010",
+		nama: "Larutan Cap Kaki Empat",
+		harga: 7500,
+		kuantitas: 8,
+	},
 ];
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const listBelanjaan = null;
+const listBelanjaan = (dataBelanjaan) => {
+	const daftarBelanja = [];
+
+	for (let index = 0; index < dataBelanjaan.length; index++) {
+		const data = dataBelanjaan[index];
+
+		daftarBelanja.push("- " + data.nama + " x " + data.kuantitas);
+	}
+
+	return daftarBelanja;
+};
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const totalBelanjaan = null;
+const totalBelanjaan = (dataBelanjaan) => {
+  let totalBelanja = 0;
+
+  for (let index = 0; index < dataBelanjaan.length; index++) {
+    const item = dataBelanjaan[index];
+    const harga = item.harga * item.kuantitas;
+    
+    totalBelanja += harga;
+  }
+
+  return totalBelanja;
+};
 
 // ! JANGAN DIMODIFIKASI
 const main = () => {
-  console.log("Selamat datang customer");
+	console.log("Selamat datang customer");
 
-  console.log("Belanjaan Anda adalah:");
-  console.log(listBelanjaan?.(dataBelanjaan)?.join("\n"));
-  console.log(
-    "\nTotal Belanjaan Anda adalah Rp. " + totalBelanjaan?.(dataBelanjaan)
-  );
+	console.log("Belanjaan Anda adalah:");
+	console.log(listBelanjaan?.(dataBelanjaan)?.join("\n"));
+	console.log(
+		"\nTotal Belanjaan Anda adalah Rp. " + totalBelanjaan?.(dataBelanjaan)
+	);
 };
 
 main();
 
 module.exports = {
-  dataBelanjaan,
-  listBelanjaan,
-  totalBelanjaan,
+	dataBelanjaan,
+	listBelanjaan,
+	totalBelanjaan,
 };
